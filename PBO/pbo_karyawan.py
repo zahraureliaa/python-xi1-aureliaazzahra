ebf3fdcacf57c81d454cbad9338d3c5c9b0a7bd1
zahraureliaa <zahraureliaaa@gmail.com>
2021-04-15 09:47:10 +0700

    PBO

--- PBO/pbo_karyawan.py
+++ PBO/pbo_karyawan.py
@@ -1,19 +1,21 @@
-class Karyawan:
+class karyawan:
     ''' Dasar kelas untuk semua karyawan'''
     jumlah_karyawan = 0
 
-    def __init__(self, nama, gaji, umur, alamat):
+    def __init__(self, nama, gaji, tahun_kelahiran, umur, alamat):
         self.nama = nama
         self.gaji = gaji
+        self.tahun_kelahiran = tahun_kelahiran
         self.umur = umur
         self.alamat = alamat
-        Karyawan.jumlah_karyawan += 1
+        karyawan.jumlah_karyawan += 1
 
     def tampilkan_jumlah(self):
-        print("Total karyawan:", Karyawan.jumlah_karyawan)
+        print("Total karyawan:", karyawan.jumlah_karyawan)
 
     def tampilkan_profil(self):
         print("Nama :", self.nama)
+        print("Tahun kelahiran :", self.tahun_kelahiran)
         print("Umur :", self.umur)
     
     def tampilkan_gaji(self):
@@ -23,19 +25,30 @@ class Karyawan:
     def tampilkan_lengkap(self):
         print("Nama :", self.nama)
         print("Alamat :", self.alamat)
+        print("Tahun kelahiran :", self.tahun_kelahiran)
         print("Umur :", self.umur)
         print("Gaji :", self.gaji)
 
-# Membuat objek pertama dari kelas Karyawan
-karyawan1 = Karyawan("Sarah", 1000000, 28, "Bantul")
+# Membuat objek pertama dari kelas karyawan
+karyawan1 = karyawan("Chan", 2500000, 1997, 24, "Bantul")
 
-# Membuat objek kedua dari kelas Karyawan
-karyawan2 = Karyawan("Budi", 2000000, 31, "Sleman")
+# Membuat objek kedua dari kelas karyawan
+karyawan2 = karyawan("Lee know", 2000000, 1998, 23, "Sleman")
 
-# Membuat ojek ketiga
-karyawan3 = Karyawan("Fika", 1500000, 26, "Gunung kidul") 
+# Membuat ojek ketiga dan seterusnya
+karyawan3 = karyawan("Changbin", 2200000, 1999, 22, "Gunung kidul") 
+karyawan4 = karyawan("Hyunjin", 1900000, 2000, 21, "Kota Yogyakarta") 
+karyawan5 = karyawan("Han", 2100000, 2000, 21, "Sleman") 
+karyawan6 = karyawan("Felix", 1900000, 2000, 21, "Sedayu") 
+karyawan7 = karyawan("Seungmin", 1800000, 2000, 21, "Bantul") 
+karyawan8 = karyawan("I.N", 1700000, 2001, 20, "Seturan") 
 
 karyawan1.tampilkan_lengkap()
 karyawan2.tampilkan_lengkap()
 karyawan3.tampilkan_lengkap()
-print("Total Karyawan :", Karyawan.jumlah_karyawan)
+karyawan4.tampilkan_lengkap()
+karyawan5.tampilkan_lengkap()
+karyawan6.tampilkan_lengkap()
+karyawan7.tampilkan_lengkap()
+karyawan8.tampilkan_lengkap()
+print("Total karyawan :", karyawan.jumlah_karyawan)
